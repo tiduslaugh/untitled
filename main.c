c91f12a0b4dbade1c7f9e6ee55c4e0c518094f5e
Alex Newton <alexsnewton@yahoo.com>
2022-05-17 23:57:39 -0400

    reformat for giggles

--- main.c
+++ main.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 #include <libguile.h>
 #include <curses.h>
+#include <assert.h>
 
-void* register_functions(void* data) {
+void *register_functions(void *data) {
+    SCM result = scm_c_eval_string("(+ 2 2)");
+    assert(scm_is_integer(result));
+    printf("%d\n", scm_to_int(result));
     return NULL;
+}
+
+void init_curses() {
+    WINDOW *window = initscr();
+    cbreak();
+    noecho();
+    intrflush(window, FALSE);
+    keypad(window, true);
 }
 
 int main(int argc, char **argv) {
     printf("Hello, World!\n");
     scm_with_guile(register_functions, NULL);
 
-    WINDOW* window = initscr();
-    cbreak();
-    noecho();
-    intrflush(window, FALSE);
-    keypad(window, true);
-
-    while(true) {
+    while (true) {
         int ch = getch();
         if (ch == 'q') {
             break;
