738290ad20d32cd80bc13657fd558da1b6034607
tiduslaugh <tiduslaugh@github.com>
2022-05-18 21:10:19 -0400

    Fix assorted horrible memory bugs (already...)

--- src/main.c
+++ src/main.c
@@ -10,53 +10,57 @@
 typedef int error_t;
 
 error_t find_root_dir(char** out_buf) {
-    char* buf = malloc(1200); // some extra to add to the path
+    char buf[1024];
+    char filebuf[1200]; // some extra to add to the path
     struct stat stats;
     error_t return_value;
-    if (buf == NULL) {
-        return ENOMEM;
-    }
+
     while(1) {
         char* cwd = getcwd(buf, 1024);
         if (cwd == NULL) {
             return_value = errno;
-            free(buf);
             return return_value;
         }
         printf("Checking path %s...\n", cwd);
         if (strcmp(cwd, "/") == 0) {
             // we've reached the root, better stop searching
             return_value = errno;
-            free(buf);
             return ENOENT;
         }
-        char* filepath = strdup(cwd);
-        strcat(filepath, "/config.scm");
-        int status = stat(filepath, &stats);
-        free(filepath);
+        strncpy(filebuf, cwd, sizeof(filebuf));
+        strncat(filebuf, "/config.scm", sizeof(filebuf) - strlen(filebuf) - 1);
+        int status = stat(filebuf, &stats);
         if(status == 0) {
             // file exists, off we go
             printf("Directory found: %s\n", cwd);
-            *out_buf = cwd;
+            *out_buf = strdup(cwd);
             return 0;
         }
         chdir("..");
     }
 }
 
-void *load_config(void *data) {
+void * load_config_internal(void* raw_path) {
+    const char *path = (const char*)raw_path;
+    SCM s = scm_c_primitive_load(path);
+    char *representation = scm_to_locale_string(scm_object_to_string(s, SCM_UNDEFINED));
+    printf("scheme object is %s\n", representation);
+    free(representation);
+}
+
+error_t load_config() {
     char *root_dir;
     error_t err = find_root_dir(&root_dir);
     if (err) {
-        return NULL;
+        return err;
     }
-    char *file_path = strdup(root_dir);
+    char *file_path = calloc(strlen(root_dir)+sizeof("/config.scm"), sizeof(char)); // sizeof string includes \0
+    strcpy(file_path, root_dir);
     strcat(file_path, "/config.scm");
-    SCM s = scm_c_primitive_load(file_path);
 
-    char *rep = scm_to_locale_string(scm_object_to_string(s, SCM_UNDEFINED));
-    printf("Scheme object is %s\n", rep);
-    free(rep);
+    scm_with_guile(load_config_internal, file_path);
+
+    return 0;
 }
 
 void *register_functions(void *data) {
@@ -76,7 +80,11 @@ void init_curses() {
 
 int main(int argc, char **argv) {
     printf("Hello, World!\n");
-    scm_with_guile(load_config, NULL);
+    error_t retval = load_config();
+    if(!retval) {
+        printf("Config loading failed with code %d\n", retval);
+        return retval;
+    }
     scm_with_guile(register_functions, NULL);
 
     while (true) {
